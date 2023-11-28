b995751eda0c7d4232fd3ccf1b687e2baa3bcd17
Evgeniy <frolov999@yandex.ru>
2023-11-28 20:12:21 +0300

    settings.py

--- settings.py
+++ settings.py
@@ -1,25 +1,25 @@
 class Settings:
-    chrome_driver = "C:\chromedriver\chromedriver.exe"
+    chrome_driver = "C:\test\chromedriver.exe"
     base_url = 'https://b2c.passport.rt.ru'
-    valid_email = 'super.presentashion@yandex.ru'
-    valid_password = 'Rostelecom721'
-    valid_email_for_reg = 'super.presentashion@yandex.ru'
-    invalid_email = 'rostelek@gmail.com'
-    invalid_password = 'Uid125777nklP89683'
+    valid_email = 'frolov999@yandex.ru'
+    valid_password = 'XywLF98q7q-Qd#B'
+    valid_email_for_reg = 'frolov999@yandex.ru'
+    invalid_email = 'test@mail.ru'
+    invalid_password = 'XywLF98q7'
     empty_email = ''
     empty_password = ''
     menu_of_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
     placeholder_name_of_user = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
-    f_name = 'Сергей'
-    l_name = 'Петров'
+    f_name = 'Евгений'
+    l_name = 'Фролов'
     f_name_ = '-'
     l_name_ = '-'
-    russian_generate_string = 'к'
-    latin_generate_string = 'ljuyejf'
+    russian_generate_string = 'строка'
+    latin_generate_string = 'genert'
     chinese_chars = '我不会说汉怎么样你会说英语'
-    special_chars = '!@#${}$*****%^&*(#)_+_+=//\[\}]]]'
-    numbers = 173
+    special_chars = '!@#{$*%^&(#)_+=/\[}]'
+    numbers = 159
     empty = ''
-    passw1 = 'Qw987654321qw'
-    passw2 = 'Uid125777nklP89683'
-    passw3 = 'IOdd9876543210ELo8964776'
+    passw1 = 'Svbejge;)9y754'
+    passw2 = 'XywLF98q7q-Qd#'
+    passw3 = 'O3210ELo8964776njsdd65546'
